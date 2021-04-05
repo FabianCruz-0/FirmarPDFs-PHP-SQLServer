5abe65f995fb6e86cf5281849df1e575add2af03
FabianCruz-0 <fabiancruz3773@gmail.com>
2021-04-05 00:57:18 -0500

    YA TOMA LOS 3 ARCHIVOS, LOS SUBE Y LEE EL PDF DEL SERVIDOR

--- insertDocs.php
+++ insertDocs.php
@@ -1,20 +1,20 @@
 <?php
 
 $nombrePDF = $_FILES['archivo-pdf']['name'];
-/* $nombreKey = $_FILES['archivo-key']['name'];
-$nombreCer = $_FILES['archivo-cer']['name']; */
+$nombreKey = $_FILES['archivo-key']['name'];
+$nombreCer = $_FILES['archivo-cer']['name'];
 
 $tipoPDF = $_FILES['archivo-pdf']['type'];
-/* $tipoKey = $_FILES['archivo-key']['type'];
-$tipoCer = $_FILES['archivo-cer']['type']; */
+$tipoKey = $_FILES['archivo-key']['type'];
+$tipoCer = $_FILES['archivo-cer']['type'];
 
 $sizePDF = $_FILES['archivo-pdf']['size'];
-/* $sizeKey = $_FILES['archivo-key']['size'];
-$sizeCer = $_FILES['archivo-cer']['size']; */
+$sizeKey = $_FILES['archivo-key']['size'];
+$sizeCer = $_FILES['archivo-cer']['size'];
 
 $ubicationPDF = $_FILES['archivo-pdf']['tmp_name'];
-/* $ubicationKey = $_FILES['archivo-key']['tmp_name'];
-$ubicationCer = $_FILES['archivo-cer']['tmp_name']; */
+$ubicationKey = $_FILES['archivo-key']['tmp_name'];
+$ubicationCer = $_FILES['archivo-cer']['tmp_name'];
 
 $srv = $_SERVER['DOCUMENT_ROOT'];
 $trim = trim($srv, "htdocs");
@@ -24,8 +24,8 @@ $carpetaDestino = $trim . 'tmp/';
 
 
 move_uploaded_file($ubicationPDF,$carpetaDestino.$nombrePDF);
-/* move_uploaded_file($ubicationKey,$carpetaDestino.$nombreKey);
-move_uploaded_file($ubicationCer,$carpetaDestino.$nombreCer); */
+move_uploaded_file($ubicationKey,$carpetaDestino.$nombreKey);
+move_uploaded_file($ubicationCer,$carpetaDestino.$nombreCer);
 
     $serverName = "DESKTOP-0HN87OP\SQLEXPRESS";
 
@@ -70,16 +70,18 @@ move_uploaded_file($ubicationCer,$carpetaDestino.$nombreCer); */
     $content64PDF = base64_encode($contentPDF);
     fclose($PDF2upload);
 
-    /* $Key2upload = fopen($carpetaDestino.$nombreKey, "r");
+    $Key2upload = fopen($carpetaDestino.$nombreKey, "r");
     $contentKey = fread($Key2upload, $sizeKey);
+    $content64Key = base64_encode($contentKey);
     fclose($Key2upload);
 
     $Cer2upload = fopen($carpetaDestino.$nombreCer, "r");
     $contentCer = fread($Cer2upload, $sizeCer);
-    fclose($Cer2upload); */
+    $content64Cer = base64_encode($contentCer);
+    fclose($Cer2upload);
 
        
-    $query = "INSERT INTO pdf (contenido) VALUES ('$content64PDF')";
+    $query = "INSERT INTO archivos (contentPDF,contentKey,contentCer) VALUES ('$content64PDF','$content64Key','$content64Cer')";
     /* 
     archID int NOT NULL PRIMARY KEY identity(1,1),
     nombrePDF text NOT NULL,
@@ -95,11 +97,11 @@ move_uploaded_file($ubicationCer,$carpetaDestino.$nombreCer); */
     */
     $stmt = sqlsrv_query($conn, $query);
 
-    $query = "SELECT * FROM pdf";
+    $query = "SELECT * FROM archivos";
     $stmt = sqlsrv_query($conn, $query);
 
     while( $row = sqlsrv_fetch_array( $stmt, SQLSRV_FETCH_ASSOC) ) {
-        $pdfServer = $row['contenido'];
+        $pdfServer = $row['contentPDF'];
   }
 
    
